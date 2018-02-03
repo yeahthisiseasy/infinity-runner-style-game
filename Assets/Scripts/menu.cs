e1c8be144007dbf66cf3f1b485d53dd52fcd9a3a
Nikita Podelwski <dxkkxd@work.com>
2018-02-03 13:16:58 +0100

    Create menu
    I create menu.

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+
+    public string level;
+    public string options;
+    public string Return;
+
+    public void start()
+    {
+        Application.LoadLevel(level);
+        
+    }
+
+    public void Options()
+    {
+        Application.LoadLevel(options);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/menu_options.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_options : MonoBehaviour {
+
+    public string Return;
+
+    public void back()
+    {
+        Application.LoadLevel(Return);
+    }
+}
