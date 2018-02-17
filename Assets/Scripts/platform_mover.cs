f7f5c8d8d5866d2f8630ee5f74cdfc8170535ee1
Nikita Podelwski <dxkkxd@work.com>
2018-02-17 18:04:22 +0100

    Add platforms movement and score
    Add platforms movement and score

---
+++ Assets/Scripts/platform_mover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform_mover : MonoBehaviour {
+
+    public float speed;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.Translate(Vector3.left * (speed * Time.deltaTime));
+
+        if (transform.localPosition.x <= -10.5)
+        {
+            Vector3 newPos = new Vector3(transform.position.x + 39f, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+	}
+}
---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+
+    public float playerscore = 0;
+    public int fontSize;
+
+    void Update()
+    {
+        playerscore += Time.deltaTime;
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        playerscore += amount;
+    }
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        GUI.Label(new Rect(10, 10, 100, 20), "Score: " + (playerscore * 10));
+    }
+
+}
